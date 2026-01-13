cce698ae76515f581cb2d449b5247ba3b06115cd
Juliana <juhomeofc@gmail.com>
2026-01-12 21:24:25 -0300

    Required e verificaçoes adicionadas

--- lib/model/expense/expense_model.dart
+++ lib/model/expense/expense_model.dart
@@ -4,9 +4,8 @@ class Expense {
   final String name;
   final double value;
   final String responsible;
-  final String card;
   final int parcels;
   RxInt paidParcels = 0.obs;
 
-  Expense(this.name, this.value, this.responsible, this.card, this.parcels);
+  Expense(this.name, this.value, this.responsible, this.parcels);
 }
